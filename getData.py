8b8450a1af1b7b91cb4227a572a0757766a24d85
MihirKakkar <42092170+MihirKakkar@users.noreply.github.com>
2020-09-11 10:53:56 -0400

    dataframe

--- getData.py
+++ getData.py
@@ -2,7 +2,7 @@
 import numpy
 import pandas
 from nba_api.stats.static import teams
-from nba_api.statss.endpoints import teamyearbyyearstats
+from nba_api.stats.endpoints import teamyearbyyearstats
 
 # checking favourite team
 team_dict = teams.get_teams()
@@ -10,4 +10,7 @@ rockets = [team for team in team_dict if team['nickname'] == 'Rockets']
 print(rockets)
 
 #
-team_threept_stats = teamyearbyyearstats.TeamYearByYearStats()
+team_threept_stats = teamyearbyyearstats.TeamYearByYearStats(league_id='00', team_id='1610612745')
+#, per_mode_simple='PerGame', season_type_all_star='regular', team_id='1610612745'
+team_threept_stats_df = team_threept_stats.get_data_frames()
+print(team_threept_stats_df)

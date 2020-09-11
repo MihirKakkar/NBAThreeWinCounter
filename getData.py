891bc0a8daa45cc9e611ac06c21a8f1df14601a2
MihirKakkar <42092170+MihirKakkar@users.noreply.github.com>
2020-09-11 12:30:10 -0400

    Update getData.py

--- getData.py
+++ getData.py
@@ -9,8 +9,11 @@ team_dict = teams.get_teams()
 rockets = [team for team in team_dict if team['nickname'] == 'Rockets']
 print(rockets)
 
-#
+# obtaining the three stats
 team_threept_stats = teamyearbyyearstats.TeamYearByYearStats(league_id='00', team_id='1610612745')
-#, per_mode_simple='PerGame', season_type_all_star='regular', team_id='1610612745'
-team_threept_stats_df = team_threept_stats.get_data_frames()
-print(team_threept_stats_df)
+team_threept_stats_df = team_threept_stats.get_data_frames()[0]
+percent_of_shots_from_three = team_threept_stats_df['FG3A']/team_threept_stats_df['FGA']
+team_threept_stats_df.insert(34, "FG3A/FGA", percent_of_shots_from_three)
+
+# comparing to wins
+correlation_with_wins = team_threept_stats_df.plot(x='FG3A/FGA', y='WIN_PCT', style='o')

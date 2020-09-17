b2c189e7d2a57314d2daa474453800f8be50b892
MihirKakkar <42092170+MihirKakkar@users.noreply.github.com>
2020-09-17 14:21:44 -0400

    Update ThreePtWinRate.py

--- ThreePtWinRate.py
+++ ThreePtWinRate.py
@@ -12,16 +12,19 @@ print(rockets)
 
 # obtaining the three pointer stats
 team_id_arr = ['1610612737', '1610612738', '1610612739', '1610612740', '1610612741', '1610612742', '1610612743', '1610612744', '1610612745', '1610612746', '1610612747', '1610612748', '1610612749', '1610612750', '1610612751', '1610612752', '1610612753', '1610612754', '1610612755', '1610612756', '1610612757', '1610612758', '1610612759', '1610612760', '1610612761', '1610612762', '1610612763', '1610612764', '1610612765', '1610612766']
-team_threept_stats = teamyearbyyearstats.TeamYearByYearStats(league_id='00', team_id='1610612765')
+singleteam_threept_stats = teamyearbyyearstats.TeamYearByYearStats(league_id='00', team_id='1610612765')
 
-#team_threept_stats = list()
+#team_threept_stats = []
 #for i in range(len(team_id_arr)):
 #    team_threept_stats.append(teamyearbyyearstats.TeamYearByYearStats(league_id='00', team_id=team_id_arr[i]))
 
-team_threept_stats_df = team_threept_stats.get_data_frames()[0]
+#print(team_threept_stats)
+
+team_threept_stats_df = singleteam_threept_stats.get_data_frames()[0]
+    
 percent_of_shots_from_three = team_threept_stats_df['FG3A']/team_threept_stats_df['FGA']
 team_threept_stats_df.insert(34, "FG3A/FGA", percent_of_shots_from_three)
-
+all_team_three_stats = team_threept_stats_df[['FG3A/FGA', 'FG3A', 'FGA']].copy()
 
 # comparing to wins through plotting
 correlation_with_wins = team_threept_stats_df.plot(x='FG3A/FGA', y='WIN_PCT', style='o')
